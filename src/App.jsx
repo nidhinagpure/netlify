ebe3335975d100cc54e93dba77ac5aaedc44c95a
nidhinagpure <nagpurenidhi8@gmail.com>
2025-07-16 20:51:38 +0530

    login page over

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import './App.css';
 
+import { Route, Routes } from 'react-router';
+import './App.css';
 import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
 
 const App = () => {
   return (
     <div>
-      <Home />
+         <Routes>
+           <Route path='/' element={<Home />}/>
+           <Route path='/login' element={<Login />}/>
+
+         </Routes>
     </div>
   )
 }
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,39 @@
 import './Login.css';
+import logo from '../../assets/logo.png';
+import { useState } from 'react';
 
 const Login = () => {
+
+  const [signState, setSignState] = useState('Sign In');
+
   return (
-    <div>Login</div>
+    <div className='login'>
+      <img src={logo} className='login-logo' alt='' />
+      <div className='login-form'>
+        <h1>{signState}</h1>
+        <form>
+          {signState === "Sign Up" ? <input type='text' placeholder='Your name' required /> : <></>}
+          <input type='email' placeholder='Your email' required />
+          <input type='password' placeholder='Password' required />
+          <button className='login-btn'>{signState}</button>
+          <div className='form-help'>
+            <div className='remember'>
+              <input type='checkbox' />
+              <label htmlFor=''>Remember Me</label>
+            </div>
+            <p>Need Help</p>
+          </div>
+        </form>
+        <div className='form-switch'>
+          {signState === "Sign In" ? <p>New to Netflix?<span
+            onClick={() => { setSignState("Sign Up") }}>
+            Sign Up Now</span></p>
+            : <p>Already have<span
+              onClick={() => { setSignState("Sign In") }}>Sign In</span></p>}
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default Login
+export default Login;
